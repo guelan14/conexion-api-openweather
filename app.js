05a5e8631c7a2588ea561a2bad2c1f5caf146720
Miguel Angel Neumann <miguelangelneumann@gmail.com>
2023-04-11 11:14:48 -0300

    My first Commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const https=require("https");
+const bodyParser=require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({enconded: true}));
+
+
+
+app.get("/",function (req,res){
+    res.sendFile(__dirname+"/index.html")
+});
+
+app.post("/",function(req,res){
+    const query=req.body.cityName;
+    const apiKey="9c47b155508afcc43a93ef12e3593837";
+    const url= "https://api.openweathermap.org/data/2.5/weather?q="+query+",%20AR&APPID="+apiKey;
+
+    https.get(url,function(response){
+            response.on("data",function(data){
+            const weatherData = JSON.parse(data);
+            const temp=weatherData.main.temp-273;
+            const weatherDescription=weatherData.weather[0].description;
+            res.send("The temperature in Posadas is"+temp+" Grados");
+            });
+        });
+    });
+
+
+app.listen(3000,function(){
+    console.log("Server is running on port 3000");
+})
